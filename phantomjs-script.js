769477bcd3aef164b959806af6e3d42f0beadfe4
Sam Pullara <spullara@gmail.com>
2017-06-14 13:17:10 -0700

    return the results as a json object

--- phantomjs-script.js
+++ phantomjs-script.js
@@ -1,20 +1,27 @@
 const page = require('webpage').create();
 const system = require('system');
 
+// Need proper error handling with an error json node
+phantom.onError = function (msg, trace) {
+    console.log(JSON.stringify({"error": msg}));
+    phantom.exit();
+};
+
 page.open(system.args[1], function(status) {
     if (status !== 'success') {
-        console.log('Unable to access network');
+        console.log(JSON.stringify({"error": status}));
     } else {
-        page.evaluate(function () {
-            const nodes = document.evaluate(system.args[2], document, null, XPathResult.ANY_TYPE, null);
-            let results = nodes.iterateNext();
-            let text = "";
-            while (results) {
-                text += results.textContent + "\n";
-                results = nodes.iterateNext();
+        const text = page.evaluate(function (xpath) {
+            // Array of results
+            var results = [];
+            // Grab the XPath from the resuling page
+            var xpathResult = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+            for (var nodes = xpathResult.iterateNext(); nodes; nodes = xpathResult.iterateNext()) {
+                results.push(nodes.outerHTML);
             }
-            console.log(text);
-        });
+            return JSON.stringify({"nodes":results});
+        }, system.args[2]);
+        console.log(text);
     }
     phantom.exit();
 });
