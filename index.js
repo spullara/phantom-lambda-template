a765e07393f0c8737787511a5d6a7f42908a3cc8
Sam Pullara <spullara@gmail.com>
2017-06-14 13:48:11 -0700

    stringify the event

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ var AWS = require('aws-sdk');
 var phantomjs = require('phantomjs-prebuilt');
 
 exports.handler = function(event, context, callback) {
-    console.log("Event: " + event);
+    console.log("Event: " + JSON.stringify(event));
+    if (event.url && event.xpath) {
+        var phantom = phantomjs.exec('phantomjs-script.js', event.url, event.xpath);
 
-    var phantom = phantomjs.exec('phantomjs-script.js', event.url, event.xpath);
+        var result = "";
 
-    var result = "";
+        phantom.stdout.on('data', function(buf) {
+            result += buf;
+        });
+        phantom.stderr.on('data', function(buf) {
+            console.log('ERROR: %s', String(buf));
+        });
+        phantom.on('close', function(code) {
+            console.log('done', code);
+        });
 
-    phantom.stdout.on('data', function(buf) {
-        result += buf;
-    });
-    phantom.stderr.on('data', function(buf) {
-        console.log('ERROR: %s', String(buf));
-    });
-    phantom.on('close', function(code) {
-        console.log('done', code);
-    });
-
-    phantom.on('exit', code => {
-        callback(null, result);
-    });
-
+        phantom.on('exit', code => {
+            callback(null, result);
+        });
+    } else {
+        callback(JSON.stringify({"error": "url and xpath must be present"}))
+    }
 };
