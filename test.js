b0808042ab6764053180a8ca8a829717de1e42a7
Sam Pullara <spullara@gmail.com>
2018-05-21 14:21:51 -0700

    output the error message

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const pjs = require('./index.js');
 
 pjs.handler({"url": "https://www.yahoo.com", "xpath": "//img"}, {}, (error, results) => {
+    console.log(error);
     console.log(results);
 });
 
